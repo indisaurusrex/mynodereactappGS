18619f73dc7e46efa52748ceceec902b22d368d9
India Rex <face940@gmail.com>
2021-01-09 13:05:33 +0000

    First styled component added, styling in _app.js and outcome on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { Page, TextStyle } from '@shopify/polaris';
+
 const Index = () => (
-    <div>
-        <p>Sample app using react and next.js</p>
-    </div>
-)
+    <Page>
+        <TextStyle variation="positive">
+        Sample app using react and next.js
+        </TextStyle>
+    </Page>
+);
 
 export default Index;
